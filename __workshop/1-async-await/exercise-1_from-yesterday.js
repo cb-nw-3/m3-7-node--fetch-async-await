404af71c6d4001c57a777d6e1dc93610891280c7
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-06-22 19:45:21 -0400

    ex1 done

--- __workshop/1-async-await/exercise-1_from-yesterday.js
+++ __workshop/1-async-await/exercise-1_from-yesterday.js
@@ -6,7 +6,32 @@ const request = require('request-promise');
 
 // Returns the current position of the ISS
 const getIssPosition = async () => {
-    // add code here (include a try/catch as well)
+    try {
+        const data = await request('http://api.open-notify.org/iss-now.json');
+        const issPosition = JSON.parse(data);
+        const result = {
+            lat: issPosition.iss_position.latitude,
+            lng: issPosition.iss_position.longitude,
+        };
+        console.log(result);
+        return result;
+    } catch (err) {
+        console.log(`The following error occured: ${err}`);
+    }
 }
 
 getIssPosition();
+
+// function getIssPosition() {
+//     return rp('http://api.open-notify.org/iss-now.json')
+//     .then(response => {
+//         const issPosition = JSON.parse(response);
+
+//         return {
+//             lat: issPosition.iss_position.latitude,
+//             lng: issPosition.iss_position.longitude,
+//         }
+//     })
+// }
+
+// getIssPosition().then((result => console.log(result)));
