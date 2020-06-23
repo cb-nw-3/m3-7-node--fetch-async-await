f866343748a15797ab3cc6e2a45d0c8a007c187c
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-06-22 21:46:05 -0400

    ex5 completed

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,20 @@
-const request = require('request-promise');
+const rp = require("request-promise");
 
 const getGeekJoke = async () => {
+  try {
+    const options = {
+      uri: "https://geek-jokes.sameerkumar.website/api?format=json",
+      headers: { Accept: "application/json" },
+    };
 
-}
+    let data = await rp(options);
+    //console.log(JSON.parse(data));
+    let joke = JSON.parse(data).joke;
+    console.log(joke);
+    return joke;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getGeekJoke();
