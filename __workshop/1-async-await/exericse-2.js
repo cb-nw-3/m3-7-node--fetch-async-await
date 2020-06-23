e27b9d7bd1e09b1eac7dbd7aa8b008d44ee7eac9
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-06-22 20:30:20 -0400

    complete exercise 0-5

--- __workshop/1-async-await/exericse-2.js
+++ __workshop/1-async-await/exericse-2.js
@@ -1,2 +1,23 @@
 // Exercise 2
 // ----------
+
+const doublesLater = async (num) => {
+  //setTimeout(2000, () => num * 2);
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(num * 2);
+    }, 1000);
+  });
+};
+
+const addPromise = async (num) => {
+  const firstNumber = await doublesLater(10);
+  const secondNumber = await doublesLater(20);
+  const thirdNumber = await doublesLater(30);
+
+  const answer = num + firstNumber + secondNumber + thirdNumber;
+  return answer;
+};
+
+addPromise(10).then((answer) => console.log(answer));
