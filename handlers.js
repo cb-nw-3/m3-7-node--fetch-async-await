6c13a1baec8a16abb66d4b3bc0ac25fde284308e
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-06-22 21:57:37 -0400

    added default endpoint and handler

--- handlers.js
+++ handlers.js
@@ -1,1 +1,15 @@
 // nothing here yet...
+
+const { getDadJoke } = require("./__workshop/2-promises-in-action/exercise-3");
+const {
+  getTronaldDump,
+} = require("./__workshop/2-promises-in-action/exercise-4");
+
+const { getGeekJoke } = require("./__workshop/2-promises-in-action/exercise-5");
+
+const handleJoke = async (req, res, next) => {
+  let joke = await getDadJoke();
+  res.status(200).send(`${joke}`);
+};
+
+module.exports = { handleJoke };
--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8000;
 
+const { handleJoke } = require("./handlers");
+
 express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-	.use(morgan('dev'))
-	.use(express.static('public'))
-    .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
-    .set('view engine', 'ejs')
+  .use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(morgan("dev"))
+  .use(express.static("public"))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .set("view engine", "ejs")
 
-    // endpoints
+  // endpoints
+  .use("/", handleJoke)
 
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on port ${PORT}`));
