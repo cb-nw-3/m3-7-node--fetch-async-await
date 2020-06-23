8fe934814d04ff4db2a8139c6869fe60eaa6fca6
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2020-06-22 20:56:38 -0400

    exercise 0 to 2

--- __workshop/1-async-await/exercise-1_from-yesterday.js
+++ __workshop/1-async-await/exercise-1_from-yesterday.js
@@ -7,6 +7,17 @@ const request = require('request-promise');
 // Returns the current position of the ISS
 const getIssPosition = async () => {
     // add code here (include a try/catch as well)
+    try {
+        const result = await request('http://api.open-notify.org/iss-now.json');
+        const location = JSON.parse(result);
+        const data = {
+            lat: location.iss_position.latitude,
+            lng: location.iss_position.longitude,
+        }
+        console.log(data)
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 getIssPosition();
--- __workshop/1-async-await/exericse-2.js
+++ __workshop/1-async-await/exericse-2.js
@@ -1,2 +1,29 @@
 // Exercise 2
 // ----------
+const doublesLater = (num) => {
+    return new Promise((resolve, reject) => {
+        console.log("TypeOf: ", typeof num, " Condition: ", (typeof num === 'number'))
+        if (typeof num === 'number') {
+            setTimeout(() => {
+                resolve(num * 2);
+            }, 2000);
+        } else {
+            reject('Error: It is not a number!')
+        }
+    })
+}
+
+const addPromise = async (num) => {
+    try {
+        const a = await doublesLater(10);
+        const b = await doublesLater(20);
+        const c = await doublesLater(30);
+        console.log(num + a + b + c);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+addPromise(10).then((sum) => {
+    console.log(sum);
+});
