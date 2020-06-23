592cfcb49b3e31fd029fa81829a69d7fd5cb84ff
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-06-22 20:50:23 -0400

    ex3 done

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,6 +1,19 @@
 const request = require('request-promise');
 
+const obj = {
+    uri: 'https://icanhazdadjoke.com',
+    headers: {
+        'Accept': 'application/json',
+    },
+    json: true,
+};
+
 const getDadJoke = async () => {
+    const getJoke = await request(obj);
+    const { joke } = getJoke;
+    console.log(joke);
 
     
 }
+
+getDadJoke();
