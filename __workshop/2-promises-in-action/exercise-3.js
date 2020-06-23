c04f495d3e86b2c69b237b663de7ac5bf441bff5
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-06-22 20:58:34 -0400

    ex3 put everything together in the function

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,14 +1,16 @@
 const request = require('request-promise');
 
-const obj = {
-    uri: 'https://icanhazdadjoke.com',
-    headers: {
-        'Accept': 'application/json',
-    },
-    json: true,
-};
+
 
 const getDadJoke = async () => {
+    const obj = {
+        uri: 'https://icanhazdadjoke.com',
+        headers: {
+            'Accept': 'application/json',
+        },
+        json: true,
+    };
+    
     const getJoke = await request(obj);
     const { joke } = getJoke;
     console.log(joke);
