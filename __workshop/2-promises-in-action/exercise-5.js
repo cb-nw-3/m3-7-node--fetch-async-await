52900a222dd26f4f84df6696dda8713e52442b1f
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-06-22 21:25:44 -0400

    ex5 done

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -2,4 +2,22 @@ const request = require('request-promise');
 
 const getGeekJoke = async () => {
 
+
+const options = {
+    url: 'https://geek-jokes.sameerkumar.website/api?format=json',
+    headers: {
+        'Accept': 'application/json',
+    },
+    json: true,
 }
+
+try {
+    const getJoke = await request(options)
+    const { joke } = getJoke;
+    console.log(joke);
+} catch (error) {
+    console.log('Error:', error);
+}
+}
+
+getGeekJoke();
