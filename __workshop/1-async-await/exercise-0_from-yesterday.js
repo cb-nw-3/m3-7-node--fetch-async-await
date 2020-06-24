864692cc2cf4a254f1855edce109fc97f644d8b0
jenny ma <jenny_lau_8@hotmail.com>
2020-06-24 18:33:28 -0400

    E0

--- __workshop/1-async-await/exercise-0_from-yesterday.js
+++ __workshop/1-async-await/exercise-0_from-yesterday.js
@@ -16,6 +16,12 @@ const compareToTen = (num) => {
 
 const myFunc = async (num) => {
     // add code here
+    try {
+        const result = await compareToTen(num);
+        console.log(result);
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 myFunc(15);
