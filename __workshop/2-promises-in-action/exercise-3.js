7ed6a7a09f7a1e8149b0d7f3eebb6267e224019b
donnyesq <adonis@darmas.ca>
2020-06-23 23:41:06 -0400

    exercise 3 complete

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,6 +1,17 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getDadJoke = async () => {
+  const options = {
+    uri: "https://icanhazdadjoke.com/",
+    headers: {
+      "User-Agent": "Request-Promise",
+      Accept: "application/json",
+    },
+    json: true,
+  };
+  const randomJoke = request(options);
 
-    
-}
+  return randomJoke;
+};
+
+getDadJoke().then((res) => console.log(res.joke));
