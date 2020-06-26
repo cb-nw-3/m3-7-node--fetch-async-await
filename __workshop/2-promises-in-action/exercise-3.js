7066e2f8b444c9c9ff6138de830080b3f8ddcd5a
jenny ma <jenny_lau_8@hotmail.com>
2020-06-25 20:39:25 -0400

    E3

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,6 +1,16 @@
 const request = require('request-promise');
 
 const getDadJoke = async () => {
+    const options = {
+        uri: 'https://icanhazdadjoke.com/',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+    const joke = await request(options);
 
+    return JSON.parse(joke);
     
 }
+
+getDadJoke().then(joke => console.log(joke));
