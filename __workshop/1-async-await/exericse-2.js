fad43edbcc726e2045eeb0441b2042a35cdeee70
Roger Lam <rogerlam@wylco.org>
2020-06-25 20:47:08 -0400

    ex 2 kinda complete

--- __workshop/1-async-await/exericse-2.js
+++ __workshop/1-async-await/exericse-2.js
@@ -1,2 +1,22 @@
 // Exercise 2
 // ----------
+
+// why does this work, when async and await don't?
+
+const doublesLater = (num) => {
+    let doubledNum = new Promise(resolve => {
+        setTimeout(() => { resolve(num * 2) }, 2000);
+    })
+    return doubledNum;
+}
+
+const addPromise = async (num) => {
+    let num1 = await doublesLater(10);
+    let num2 = await doublesLater(20);
+    let num3 = await doublesLater(30);
+    return (num + num1 + num2 + num3);
+}
+
+addPromise(10).then(val => {
+    console.log(val);
+});
