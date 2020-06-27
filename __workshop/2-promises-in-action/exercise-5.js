0760c5460ec30252cfa8cc440923d2dceb3dbe52
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-27 14:08:41 -0400

    exercise 5 solved

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,12 @@
 const request = require('request-promise');
+const { response } = require('express');
 
 const getGeekJoke = async () => {
-
-}
+  const response = await request(
+    'https://geek-jokes.sameerkumar.website/api?format=json'
+  );
+  //   console.log('hey', response);
+  const data = await JSON.parse(response);
+  console.log(data.joke);
+};
+getGeekJoke();
