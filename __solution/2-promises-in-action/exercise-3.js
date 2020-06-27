c06f257ef7f242aeef3df61e3a4db1fa09ddf1fe
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-27 12:35:02 -0400

    exercise 3

--- __solution/2-promises-in-action/exercise-3.js
+++ __solution/2-promises-in-action/exercise-3.js
@@ -1,17 +1,16 @@
 const request = require('request-promise');
 
 const getDadJoke = async () => {
-    const options = {
-        uri: 'https://icanhazdadjoke.com/',
-        headers: {
-            'Accept': 'application/json'
-        }
-    }
-    const joke = await request(options);
+  const options = {
+    uri: 'https://icanhazdadjoke.com/',
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  const joke = await request(options);
 
-    return JSON.parse(joke);
-}
+  return JSON.parse(joke);
+};
 
-// getJoke().then(data => console.log(data));
-
-module.exports = { getDadJoke };
+getDadJoke().then((data) => console.log(data));
+// module.exports = { getDadJoke };
--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,6 +1,14 @@
 const request = require('request-promise');
-const fetch = requiere('node-fetch');
+// const fetch = requiere('node-fetch');
 
 const getDadJoke = async () => {
-  return await request('https://icanhazdadjoke.com/');
+  const options = {
+    uri: 'https://icanhazdadjoke.com/',
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  return JSON.parse(await request(options)).joke;
 };
+
+getDadJoke().then((answer) => console.log(answer));
