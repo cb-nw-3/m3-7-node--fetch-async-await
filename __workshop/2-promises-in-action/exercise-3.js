c1f86b7318d04a62467e140caa5de4b8eda874e8
David Elliott <dave.elliott@gmail.com>
2020-06-27 14:01:20 -0400

    all 2-promises-in-action done

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,6 +1,25 @@
 const request = require('request-promise');
 
-const getDadJoke = async () => {
 
-    
+console.log("remember to push this manually from the command line");
+
+const rp = require('request-promise');
+
+var dadJokeOptions = {
+    uri: 'https://icanhazdadjoke.com/',
+    headers: {
+        'Accept': 'application/json'
+    },
+    json: true // Automatically parses the JSON string in the response
+};
+
+
+const getDadJoke = async () => {
+    try 
+    {
+        let joke = await rp(dadJokeOptions);
+        console.log (joke.joke);
+    } catch (err) { console.log (err); }
 }
+
+getDadJoke();
--- __workshop/2-promises-in-action/exercise-4.js
+++ __workshop/2-promises-in-action/exercise-4.js
@@ -1,5 +1,28 @@
 const request = require('request-promise');
 
+
+console.log("remember to push this manually from the command line");
+
+const rp = require('request-promise');
+
+var trumpQuoteOptions = {
+    uri: 'https://api.tronalddump.io/random/quote',
+    headers: {
+        'Accept': 'application/json'
+    },
+    json: true // Automatically parses the JSON string in the response
+};
+
+
+
+
 const getTronaldDump = async () => {
+    try 
+    {
+        let quote = await rp(trumpQuoteOptions);
+        console.log (quote.value);
+    } catch (err) { console.log (err); }
 
 }
+
+getTronaldDump();
