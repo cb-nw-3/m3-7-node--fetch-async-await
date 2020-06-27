f92fb85f1e129b2182f3a2cb2de8839738c9f51a
Roger Lam <rogerlam@wylco.org>
2020-06-27 12:23:09 -0400

    complete to front-end

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,6 +1,20 @@
 const request = require('request-promise');
 
-const getDadJoke = async () => {
-
-    
+const options = {
+    uri: 'https://icanhazdadjoke.com/',
+    headers: {
+        "Accept": "application/json"
+    },
+    json: true
 }
+
+const getDadJoke = async () => {
+    return await request(options)
+        .then(data => { return data.joke });
+}
+
+// eh doyyyyy. Yeah, you need to run the function to get it to run.
+
+// getDadJoke().then(data => { console.log(data) });
+
+module.exports = getDadJoke;
--- __workshop/2-promises-in-action/exercise-4.js
+++ __workshop/2-promises-in-action/exercise-4.js
@@ -1,5 +1,23 @@
 const request = require('request-promise');
 
-const getTronaldDump = async () => {
-
+const options = {
+    uri: 'https://tronalddump.io/random/quote',
+    headers: {
+        "Accept": "application/json"
+    },
+    json: true
 }
+
+const getTronaldDump = async () => {
+    let quote = await request(options).then(data => {
+        return data;
+    }).catch((err) => {
+        console.log(err);
+    });
+    return quote.value;
+}
+
+// getTronaldDump()
+//     .then(data => console.log(data));
+
+module.exports = getTronaldDump;
