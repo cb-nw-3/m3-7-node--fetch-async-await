2ac4b89911e015b6c43377cc2bf667e26fdd3b35
kolbymckeown <mckeownkolby@gmail.com>
2020-06-27 15:04:02 -0400

    Complete workshop m3-7

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,6 +1,16 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getDadJoke = async () => {
+	const options = {
+		uri: "https://icanhazdadjoke.com/",
+		headers: {
+			Accept: "application/json",
+		},
+	};
+	const joke = await request(options);
+	return JSON.parse(joke);
+};
+// call the joke
+getDadJoke().then(console.log);
 
-    
-}
+// try and get it so only the joke shows, not ID and status
--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,13 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getGeekJoke = async () => {
+	let options = {
+		url: "https://geek-jokes.sameerkumar.website/api?format=json",
+		method: "GET",
+	};
+	const joke = await request(options);
+	const parsed = JSON.parse(joke);
+	return parsed.joke;
+};
 
-}
+getGeekJoke().then(console.log);
