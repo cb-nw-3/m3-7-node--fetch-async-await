2f9ff93d980b1b4844130c3650057f1cd78d5379
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-27 13:24:14 -0400

    exercise 4

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,5 +1,5 @@
 const request = require('request-promise');
-// const fetch = requiere('node-fetch');
+const fetch = require('node-fetch');
 
 const getDadJoke = async () => {
   const options = {
@@ -11,4 +11,37 @@ const getDadJoke = async () => {
   return JSON.parse(await request(options)).joke;
 };
 
-getDadJoke().then((answer) => console.log(answer));
+async function wrapper() {
+  let joke = await getDadJoke();
+  console.log('request:', joke);
+}
+
+wrapper();
+
+// now using fetch
+async function asyncWrapper() {
+  try {
+    const parameter = {
+      headers: {
+        Accept: 'application/json',
+      },
+    };
+    let response = await fetch('https://icanhazdadjoke.com/', parameter);
+    let data = await response.json();
+    console.log('fetch:', data.joke);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+asyncWrapper();
+
+//without async await
+const parameter = {
+  headers: {
+    Accept: 'application/json',
+  },
+};
+fetch('https://icanhazdadjoke.com/', parameter)
+  .then((res) => res.json())
+  .then((data) => console.log(data.joke));
