d6602fa572f355f468cc5ebc0149b95c6c1a5fe1
donnyesq <adonis@darmas.ca>
2020-06-27 12:23:33 -0400

    joke selector exercise complete

--- handlers.js
+++ handlers.js
@@ -1,1 +1,52 @@
-// nothing here yet...
+const request = require("request-promise");
+
+// DAD JOKE
+const getDadJoke = async () => {
+  const options = {
+    uri: "https://icanhazdadjoke.com/",
+    headers: {
+      "User-Agent": "Request-Promise",
+      Accept: "application/json",
+    },
+    json: true,
+  };
+  const randomJoke = await request(options);
+
+  return randomJoke.joke;
+};
+
+// TRUMP QUOTE
+
+const getTronaldDump = async () => {
+  const options = {
+    uri: "https://api.tronalddump.io/random/quote",
+    method: "GET",
+    headers: {
+      "User-Agent": "Request-Promise",
+      Accept: "application/json",
+    },
+    json: true,
+  };
+  const quote = await request(options);
+
+  return quote.value;
+};
+
+// GEEK JOKE
+
+const getGeekJoke = async () => {
+  const options = {
+    uri: "https://geek-jokes.sameerkumar.website/api?format=json",
+    method: "GET",
+    headers: {
+      "User-Agent": "Request-Promise",
+      Accept: "application/json",
+    },
+    json: true,
+  };
+  const joke = await request(options);
+
+  return joke.joke;
+};
+
+module.exports = { getDadJoke, getGeekJoke, getTronaldDump };
--- public/make-me-laugh/scripts.js
+++ public/make-me-laugh/scripts.js
@@ -1,1 +1,33 @@
-// nothing here yet...
+const form = document.querySelector("form");
+const div = document.createElement("div");
+document.body.append(div);
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const type = form.type.value;
+  console.log(type);
+
+  const data = {
+    type,
+  };
+
+  fetch("/make-me-laugh", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const { status, error, joke } = data;
+      if (status === "success") {
+        div.innerHTML = joke;
+      } else if (error) {
+        console.log(error);
+      }
+    });
+};
+
+form.addEventListener("submit", handleSubmit);
