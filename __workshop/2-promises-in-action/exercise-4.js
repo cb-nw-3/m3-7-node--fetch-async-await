1f6dedf899f1600a97c323b306358ce93c082673
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-27 13:58:27 -0400

    exercise 4 solved

--- __workshop/2-promises-in-action/exercise-4.js
+++ __workshop/2-promises-in-action/exercise-4.js
@@ -1,5 +1,46 @@
 const request = require('request-promise');
+const fetch = require('node-fetch');
 
-const getTronaldDump = async () => {
+//Without async await
+const parameter = {
+  headers: {
+    Accept: 'application/json',
+  },
+};
+fetch('https://api.tronalddump.io/random/quote', parameter)
+  .then((res) => res.json())
+  .then((data) => console.log('No async:', data.value));
 
+const getTronaldDump = async () => {};
+
+//using fetch and async await
+async function asyncWrapper() {
+  const parameter = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  let response = await fetch(
+    'https://api.tronalddump.io/random/quote',
+    parameter
+  );
+  let data = await response.json();
+  console.log('async fetch: ', data.value);
 }
+
+asyncWrapper();
+
+//using request and async await
+async function asyncWrapper2() {
+  const options = {
+    uri: 'https://api.tronalddump.io/random/quote',
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  let response = await request(options);
+  let data = JSON.parse(response);
+  console.log('async request: ', data.value);
+}
+
+asyncWrapper2();
