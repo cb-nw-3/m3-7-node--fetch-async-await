7d7dc5821da0038bc858dfe15a6953f818539c2a
Przemek Piwek <przemekpiwek@gmail.com>
2020-06-28 15:43:23 -0600

    Exercise 1-2 completed

--- __workshop/1-async-await/exercise-1_from-yesterday.js
+++ __workshop/1-async-await/exercise-1_from-yesterday.js
@@ -2,11 +2,21 @@
 // -----------------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+const request = require("request-promise");
 
 // Returns the current position of the ISS
 const getIssPosition = async () => {
-    // add code here (include a try/catch as well)
-}
+  // add code here (include a try/catch as well)
+  try {
+    let data = await request("https://api.wheretheiss.at/v1/satellites/25544");
+    let coordinates = JSON.parse(data);
+    return {
+      lat: coordinates.latitude,
+      lng: coordinates.longitude,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 getIssPosition();
--- __workshop/1-async-await/exericse-2.js
+++ __workshop/1-async-await/exericse-2.js
@@ -1,2 +1,12 @@
 // Exercise 2
 // ----------
+
+const doublesLater = async (num) => {
+  let x = await setTimeout(() => {
+    return num * 2;
+  }, 2000);
+  console.log(x);
+  return x;
+};
+
+doublesLater(5);
