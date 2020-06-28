9ac88c5af1ff4e19ee9b2a8f36106d38d57b42a4
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-06-28 16:35:32 -0400

    exercise 1

--- __workshop/1-async-await/exercise-1_from-yesterday.js
+++ __workshop/1-async-await/exercise-1_from-yesterday.js
@@ -4,9 +4,27 @@
 // require the 'request-promise' module.
 const request = require('request-promise');
 
+function grabData(data) {
+  return {
+    lat: Number(data.iss_position.latitude),
+    lng: Number(data.iss_position.longitude)
+  }
+}
+function parseData(data) {
+  return JSON.parse(data)
+}
+
 // Returns the current position of the ISS
 const getIssPosition = async () => {
-    // add code here (include a try/catch as well)
+  try {
+    const data = await request('http://api.open-notify.org/iss-now.json')
+    const parsedData = JSON.parse(data)
+    const result = await grabData(parsedData)
+    console.log(result)
+    return result
+  } catch(err) {
+    console.log(err)
+  }
 }
 
 getIssPosition();
