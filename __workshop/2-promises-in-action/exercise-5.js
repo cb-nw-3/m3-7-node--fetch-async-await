1eba335be71d0abcdfe4b7847f56f1db9a0a1dff
Przemek Piwek <przemekpiwek@gmail.com>
2020-06-28 19:30:11 -0600

    Exercise 5 Complete

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,14 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getGeekJoke = async () => {
-
-}
+  try {
+    let result = await request({
+      uri: "https://geek-jokes.sameerkumar.website/api?format=json",
+      json: true,
+    });
+    return result.joke;
+  } catch (err) {
+    console.log(err);
+  }
+};
+getGeekJoke().then((res) => console.log(res));
