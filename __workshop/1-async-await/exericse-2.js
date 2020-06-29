1528c4216842414b7d6522cb2dac923b41af0f6e
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-06-29 16:45:14 -0400

    exercise 2

--- __workshop/1-async-await/exericse-2.js
+++ __workshop/1-async-await/exericse-2.js
@@ -1,2 +1,21 @@
 // Exercise 2
 // ----------
+
+function doubleAfter2Seconds(num) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(num * 2)
+    }, 2000);
+  })
+}
+
+async function addPromise(num) {
+  const firstNum = await doubleAfter2Seconds(num);
+  const secondNum = await doubleAfter2Seconds(firstNum);
+  const thirdNum = await doubleAfter2Seconds(secondNum);
+  let sum = firstNum + secondNum + thirdNum;
+  console.log(sum)
+  return sum
+}
+
+addPromise(10)
