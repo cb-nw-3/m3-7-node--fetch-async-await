fd7f0dc17f87e7619336dbfa0dfa07f5808f4954
Przemek Piwek <przemekpiwek@gmail.com>
2020-06-28 19:15:49 -0600

    exercise 3 finished

--- __workshop/2-promises-in-action/exercise-4.js
+++ __workshop/2-promises-in-action/exercise-4.js
@@ -1,5 +1,11 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getTronaldDump = async () => {
+  let quote = await request({
+    uri: "https://api.tronalddump.io/",
+    json: true,
+  });
+  console.log(quote);
+};
 
-}
+getTronaldDump();
