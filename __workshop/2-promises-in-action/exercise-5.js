b2b6949f09cabb3e2701b6b7de8c13f12df22f51
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-06-29 21:22:14 -0400

    exercise 5

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,14 @@
 const request = require('request-promise');
 
 const getGeekJoke = async () => {
+  const options = {
+    url: 'https://geek-jokes.sameerkumar.website/api?format=json',
+    method: 'GET'
+  }
 
+  const joke = await request(options);
+
+  console.log('\n' + JSON.parse(joke).joke + '\n')
 }
+
+getGeekJoke();
