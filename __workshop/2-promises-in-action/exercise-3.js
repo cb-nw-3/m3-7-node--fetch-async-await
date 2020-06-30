ee6d7fa4a6af0f1a900a0d2a13e8ee62650c7246
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-06-29 20:14:57 -0400

    exercise 3

--- __workshop/2-promises-in-action/exercise-3.js
+++ __workshop/2-promises-in-action/exercise-3.js
@@ -1,6 +1,22 @@
 const request = require('request-promise');
+const fetch = require('node-fetch');
+
+let options = {
+  uri: 'https://icanhazdadjoke.com/',
+  headers: {
+    "Accept": "application/json"
+  }
+}
 
 const getDadJoke = async () => {
-
-    
+  request(options)
+    .then(res => {
+      console.log('\n' + JSON.parse(res).joke + '\n')
+      return JSON.parse(res)
+    })
+    .catch(err => {
+      console.log('Error:', err)
+    })
 }
+
+getDadJoke();
