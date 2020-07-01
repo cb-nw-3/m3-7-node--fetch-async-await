39b5349624ce6f7e9570a95b8dd622d3da0b2df4
splashley <ashleyjlhynes@gmail.com>
2020-07-01 15:40:24 -0400

    Exercise 2

--- __workshop/1-async-await/exericse-2.js
+++ __workshop/1-async-await/exericse-2.js
@@ -1,2 +1,24 @@
 // Exercise 2
 // ----------
+
+// #1
+const doublesLater = (num) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(num * 2);
+    }, 2000);
+  });
+};
+
+// #2
+const addPromise = async (num) => {
+  const a = await doublesLater(10);
+  const b = await doublesLater(20);
+  const c = await doublesLater(30);
+
+  return num + a + b + c;
+};
+
+addPromise(10).then((sum) => {
+  console.log(sum);
+});
