617466dcb6d4592ae9d71dbdc63ef483aeaed570
splashley <ashleyjlhynes@gmail.com>
2020-07-01 19:01:20 -0400

    Exercise 5

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,16 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getGeekJoke = async () => {
+  const options = {
+    uri: "https://geek-jokes.sameerkumar.website/api?format=json",
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  const jokes = await request(options);
 
-}
+  return JSON.parse(jokes);
+};
+
+getGeekJoke().then((joke) => console.log(joke.joke));
