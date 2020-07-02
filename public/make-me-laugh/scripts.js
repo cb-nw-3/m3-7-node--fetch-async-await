b8039d258ea989458aa3c44c0c0fcd64f6cc12e2
splashley <ashleyjlhynes@gmail.com>
2020-07-02 17:35:07 -0400

    The Rest

--- public/make-me-laugh/scripts.js
+++ public/make-me-laugh/scripts.js
@@ -1,1 +1,39 @@
-// nothing here yet...
+//function for showing chosen joke type
+const updateJoke = (res) => {
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.innerHTML = res;
+};
+
+const jokeFetch = async () => {
+  let selection = userSelection.value;
+  let res;
+  switch (selection) {
+    case "dad":
+      res = await fetch("/dadjoke").then((data) => {
+        return data.text().then((response) => {
+          return response;
+        });
+      });
+      updateJoke(res);
+      break;
+    case "tronald":
+      res = await fetch("/tronald").then((data) => {
+        return data.text().then((response) => {
+          return response;
+        });
+      });
+      updateJoke(res);
+      break;
+    case "geek":
+      res = await fetch("/geekjoke").then((data) => {
+        return data.text().then((response) => {
+          return response;
+        });
+      });
+      updateJoke(res);
+      break;
+  }
+};
+
+let userSelection = document.querySelector("#type");
+userSelection.addEventListener("change", jokeFetch);
--- server.js
+++ server.js
@@ -1,23 +1,49 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { getDadJoke, getGeekJoke, getTronaldDump } = require("./handlers");
 
 const PORT = process.env.PORT || 8000;
 
+//functions
+const handleDadJoke = async (req, res) => {
+  let dadJoke = await getDadJoke();
+  res.send(dadJoke);
+};
+
+const handleTronaldDump = async (req, res) => {
+  let tronaldJoke = await getTronaldDump();
+  res.send(tronaldJoke);
+};
+
+const handleGeekJoke = async (req, res) => {
+  let geekJoke = await getGeekJoke();
+  res.send(geekJoke);
+};
+
 express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-	.use(morgan('dev'))
-	.use(express.static('public'))
-    .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
-    .set('view engine', 'ejs')
+  .use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(morgan("dev"))
+  .use(express.static("public"))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .set("view engine", "ejs")
 
-    // endpoints
+  // endpoints
+  .get("/dadjoke", handleDadJoke)
+  .get("/tronald", handleTronaldDump)
+  .get("/geekjoke", handleGeekJoke)
+  .get("*", (req, res) => {
+    res.status(400).send("ERROR 404");
+  })
 
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on port ${PORT}`));
