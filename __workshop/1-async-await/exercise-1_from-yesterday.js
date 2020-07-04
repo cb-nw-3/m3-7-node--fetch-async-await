0a593d086a58b824a9572a83d95e42c1db762fff
carldumas <dumas.carl@gmail.com>
2020-07-04 17:35:15 -0400

    M3-7: Node - 1-Async-Await - Ex. 1

--- __workshop/1-async-await/exercise-1_from-yesterday.js
+++ __workshop/1-async-await/exercise-1_from-yesterday.js
@@ -2,11 +2,28 @@
 // -----------------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+const rp = require('request-promise');
 
 // Returns the current position of the ISS
 const getIssPosition = async () => {
-    // add code here (include a try/catch as well)
+    try {
+        const response = await rp('http://api.open-notify.org/iss-now.json');
+        // console.log(response);
+
+        const issLocation = JSON.parse(response);
+        // console.log(issLocation);
+        
+        const data = {
+            lat: issLocation.iss_position.latitude,
+            lng: issLocation.iss_position.longitude,
+        }
+        
+        console.log(data);
+        return data;
+    }
+    catch (err) {
+        return console.log('Error: ', err);
+    }
 }
 
 getIssPosition();
