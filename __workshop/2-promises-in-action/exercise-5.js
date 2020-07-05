cd74cea98083644b0bb84165ac12f707eafea8a1
carldumas <dumas.carl@gmail.com>
2020-07-04 20:06:48 -0400

    M3-7: Node - 2-Promises-in-action - Ex. 5

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,22 @@
-const request = require('request-promise');
+const rp = require('request-promise');
+
+const options = {
+    uri: 'https://geek-jokes.sameerkumar.website/api?format=json',
+    headers: {
+        'Accept': 'application/json'
+    },
+    json: true,
+};
+
 
 const getGeekJoke = async () => {
-
+    try {
+        const joke = await rp(options);
+        return console.log(joke.joke)
+    }
+    catch (err) {
+        return console.log('Error: ', err);
+    }
 }
+
+getGeekJoke();
