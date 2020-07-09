a56ebec2124048795234f063ff5ba47ebf7d2d5d
Nathalie Benarroch <nathaliebenarroch@Nathalies-MacBook-Pro.local>
2020-07-08 22:59:42 -0400

    finished 7 Jokes Apis

--- public/make-me-laugh/scripts.js
+++ public/make-me-laugh/scripts.js
@@ -7,11 +7,13 @@ document
     //select an element with the id type and accesses its value attribute
     // and stores it in variable x
     const obj = { body: { type: x }, method: "POST" };
-
+    //see inserver the tfunction handlejoke that accesses the type from req obj
     const response = await fetch("/jokes", obj);
     // above calling the fetch method and  passing it parameters
     // one parameter is the /jokes address
     //the other is the request object. It defines method of request and the body of the request
+    //THIS LINE BELOW IS AT BOTTOM OF SERVER.JS/BUSINESS SET
+    //.post("/jokes", handleJoke)
 
     // put the response in json
     const { data } = await response.json();
